5d8d1cedc1b2d50403493ff8f909766e1a78ff6d
CHLMA123 <machunlei@bogon.dhcp>
2017-08-19 18:04:21 +0800

    ###巧用多字符Char(代码取巧法)
    char这个数据类型和它的使用方式：
     char c = 'a'; 这个 c 变量在 ASCII 编码下是 97。
    
     还有一种不常见的多字符 char 的写法：
     int i = 'AaBb';
     这个 i 变量的值按每个 char 的 ASCII 值转 16 进制拼在一起，也就是说：
    
     'AaBb'
     -> '0x41'+'0x61'+'0x42'+'0x62'
     -> '0x41614262' // 十进制1096893026
     PS：这个组合方式和“大小端”有关系，上面是 i386 下的结果
     多字符的长度限度为最多 4 个 char
    
     代码取巧法则一，比如：
    
     self.someButton.tag = 'SHIT';
     ...
    
     if (button.tag == 'SHIT') {
     NSLog(@"I've got this shit button");
     }
    
     当然使用tag是很不推荐的写法，尽量不用。使用这个特性来避免些魔法数字或标记些整型数字还是极好的。

--- TableViewDemo/ViewController.m
+++ TableViewDemo/ViewController.m
@@ -51,12 +51,42 @@
     [self.tableView reloadData];
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+/**
+ ###巧用多字符Char(代码取巧法)
 
-    if (_editCellIndex != -1 && _editCellIndex == indexPath.row) {
-        return 71 + 56;
+ char这个数据类型和它的使用方式：
+ char c = 'a'; 这个 c 变量在 ASCII 编码下是 97。
+ 
+ 还有一种不常见的多字符 char 的写法：
+ int i = 'AaBb';
+ 这个 i 变量的值按每个 char 的 ASCII 值转 16 进制拼在一起，也就是说：
+ 
+ 'AaBb'
+ -> '0x41'+'0x61'+'0x42'+'0x62'
+ -> '0x41614262' // 十进制1096893026
+ PS：这个组合方式和“大小端”有关系，上面是 i386 下的结果
+ 多字符的长度限度为最多 4 个 char
+ 
+ 代码取巧法则一，比如：
+ 
+ self.someButton.tag = 'SHIT';
+ ...
+ 
+ if (button.tag == 'SHIT') {
+ NSLog(@"I've got this shit button");
+ }
+ 
+ 当然使用tag是很不推荐的写法，尽量不用。使用这个特性来避免些魔法数字或标记些整型数字还是极好的。
+ */
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (tableView.tag == 'abcd') {
+        return 127;
+    }else {
+        if (_editCellIndex != -1 && _editCellIndex == indexPath.row) {
+            return 71 + 56;
+        }
+        return 71;
     }
-    return 71;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -93,6 +123,7 @@
         _tableView.delegate = self;
         _tableView.dataSource = self;
         _tableView.tableFooterView = [[UIView alloc] init];
+        _tableView.tag = 'abcd';
         [_tableView registerClass:[MTableViewCell class] forCellReuseIdentifier:@"MTableViewCell"];
     }
     return _tableView;
